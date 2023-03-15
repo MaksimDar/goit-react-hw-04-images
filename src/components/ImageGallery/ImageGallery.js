286615a88a6f5f2341ae812c1b8427e0669b8b0a
Maksim1903 <dovgusha.m@gmail.com>
2023-03-15 22:58:42 +0100

    .

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { toast } from 'react-toastify';
-
 import ImageGalleryItem from '../ImageGalleryItem';
 import Button from 'components/Button';
 import { getPhotos } from '../../services/api';
 import { ImageGalleryList } from './ImageGallery.styled';
 import Loader from '../Loader';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
-const ImageGallery = ({ queryProp }) => {
+const ImageGallery = ({ queryProp, openModal }) => {
   const [query, setQuery] = useState('');
   const [page, setPage] = useState(1);
   const [images, setImages] = useState([]);
@@ -17,9 +15,7 @@ const ImageGallery = ({ queryProp }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [imageTag, setImageTag] = useState('');
 
-  // async componentDidMount() {
-  //   this.setState({ query: this.props.query });
-  // }
+  const loadMore = useRef();
   useEffect(() => {
     setQuery(queryProp);
     setPage(1);
@@ -54,69 +50,30 @@ const ImageGallery = ({ queryProp }) => {
     };
     fetchData();
   }, [query, page]);
-
-  // async componentDidUpdate(_, prevState) {
-  //   const { query, page } = this.state;
-  //   if (query !== prevState.query) {
-  //     try {
-  //       this.setState({ isLoading: true });
-  //       const response = await getPhotos(query, page);
-  //       const totalPages = Math.ceil(response.totalHits / 12);
-
-  //       const imagesArray = response.hits;
-
-  //       this.setState({
-  //         images: imagesArray.map(
-  //           ({ id, webformatURL, largeImageURL, tags }) => ({
-  //             id,
-  //             webformatURL,
-  //             largeImageURL,
-  //             imageTag: tags,
-  //           })
-  //         ),
-  //         totalPages: totalPages,
-  //       });
-  //     } catch (error) {
-  //       console.log(error);
-  //       toast.error('Something went wrong!');
-  //     } finally {
-  //       this.setState({ isLoading: false });
-  //     }
-
-  //     return;
-  //   }
   useEffect(() => {
-    if (prevState>1) {
-      const{height:cardHeight}=
+    if (page > 1) {
+      const { height: cardHeight } = document
+        .querySelector('ul')
+        .firstElementChild.getBoundingClientRect();
+      window.scrollBy({
+        top: cardHeight * 2,
+        behavior: 'smooth',
+      });
     }
   });
-
-  //   if (prevState.page > 1) {
-  //     const { height: cardHeight } = document
-  //       .querySelector('ul')
-  //       .firstElementChild.getBoundingClientRect();
-
-  //     window.scrollBy({
-  //       top: cardHeight * 2,
-  //       behavior: 'smooth',
-  //     });
-  //   }
-
-  //   return;
-  // }
-
   const handleButtonClick = () => {
     if (totalPages === page) {
       toast.info('There is no more images');
       return;
     }
     setPage(state => state + 1);
+    loadMore.current = true;
   };
 
   const onImageClick = id => {
-    this.props.onImageClick(
-      images.find(image => image.id === +id).largeImageURL
-    );
+    const { largeImageURL, imageTag } = images.find(image => image.id === +id);
+    openModal(largeImageURL, imageTag);
+    loadMore.current = false;
   };
   return (
     <>
@@ -141,4 +98,4 @@ const ImageGallery = ({ queryProp }) => {
 
 export default ImageGallery;
 
-ImageGallery.propTypes = { query: PropTypes.string.isRequired };
+ImageGallery.propTypes = { query: PropTypes.string };
